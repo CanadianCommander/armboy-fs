6cd765b16aee8185ff26fc195a16071e66618739
Benjamin Benetti <ben.benetti@shaw.ca>
2018-05-22 19:26:02 -0700

    SD functionality!
    + R/W blocks and get card information !

--- src/sdDriver.c
+++ src/sdDriver.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <memory.h>
 #include <config.h>
+#include <util/debug.h>
 #include <hardware/hardware.h>
+
+static uint32_t cardBlockCount =0;
 
 //send standard 6 byte command & look for response
 static uint8_t sendCommand(uint8_t * cmd){
@@ -33,6 +36,24 @@ static uint8_t getNextByte(){
   uint8_t res = 0x0;
   sendSPI(&fake, &res, 1);
   return res;
+}
+
+static void writeNextByte(uint8_t byte){
+  sendSPI(&byte,NULL,1);
+}
+
+/** wait for SD to become ready **/
+static void waitForCard(){
+  while(getNextByte() != 0xFF){
+    sleep(1);
+  }
+}
+
+static bool checkAddress(uint32_t addr){
+  if(addr <= cardBlockCount){
+    return true;
+  }
+  return false;
 }
 
 //send a bunch of nops down the SPI channel
@@ -67,7 +88,12 @@ bool initDefault(void){
   setCS(0);
   uint8_t cmd0[] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
   tOut = 0;
-  while(sendCommand(cmd0) != 0x01){
+  for(;;){
+    uint8_t r1 = sendCommand(cmd0);
+    if(r1 == 0x01){
+      break;
+    }
+
     sleep(100);
     if(tOut++ > TIMEOUT_CLK){
       printf("SD Card init error CMD0 Timeout\n");
@@ -116,11 +142,52 @@ bool initDefault(void){
     return false;
   }
 
+  //get SD size
+  uint8_t csd[16];
+  memset(csd, 0, 16);
+  if(!getCardCSD(csd)){
+    printf("SD Card init error, cannot get CSD register\n");
+    return false;
+  }
+  cardBlockCount = ((((uint32_t)csd[7] & 0x3F) << 16) | ((uint16_t)csd[8] << 8) | csd[9]) + 1;
+
   return true;
 }
 
+bool getCardCSD(uint8_t * buffer){
+  uint8_t cmd9[] = {0x49, 0x00, 0x00, 0x00, 0x00, 0x01};
+
+  if(sendCommand(cmd9) == 0x00){
+    int z =0;
+    for(z=0; z < TIMEOUT_RW_BLOCK; z++){
+      if(getNextByte() == 0xFE){
+        break;
+      }
+    }
+
+    if(z != TIMEOUT_RW_BLOCK){
+      for(int i =0; i < CSD_RESPONSE_SIZE; i++){
+        buffer[i] = getNextByte();
+      }
+      waitForCard();
+      return true;
+    }
+    else {
+      waitForCard();
+      return false;
+    }
+  }
+  else {
+    waitForCard();
+    return false;
+  }
+}
 
 bool readBlock(uint8_t * buffer, uint32_t addr){
+  if(!checkAddress(addr)){
+    return false;
+  }
+
   uint8_t cmd17[6];
   cmd17[0] = 0x51;
   cmd17[1] = 0xFF & (addr >> 24);
@@ -131,13 +198,13 @@ bool readBlock(uint8_t * buffer, uint32_t addr){
 
   if(sendCommand(cmd17) == 0x00){
     int z =0;
-    for(z=0; z < TIMEOUT_CLK; z++){
+    for(z=0; z < TIMEOUT_RW_BLOCK; z++){
       if(getNextByte() == 0xFE){
         break;
       }
     }
 
-    if(z != TIMEOUT_CLK){
+    if(z != TIMEOUT_RW_BLOCK){
       //read block
       for(int i = 0; i < RW_BLOCK_SIZE; i++){
         buffer[i] = getNextByte();
@@ -147,15 +214,66 @@ bool readBlock(uint8_t * buffer, uint32_t addr){
       getNextByte();
       getNextByte();
 
+      waitForCard();
       return true;
     }
     else{
-      flushChannel();
+      waitForCard();
       return false;
     }
   }
   else {
-    flushChannel();
+    waitForCard();
+    return false;
+  }
+}
+
+
+bool writeBlock(uint8_t * buffer, uint32_t addr){
+  if(!checkAddress(addr)){
+    return false;
+  }
+
+  uint8_t cmd24[6];
+  cmd24[0] = 0x58;
+  cmd24[1] = 0xFF & (addr >> 24);
+  cmd24[2] = 0xFF & (addr >> 16);
+  cmd24[3] = 0xFF & (addr >> 8 );
+  cmd24[4] = 0xFF & (addr      );
+  cmd24[5] = 0x01;
+
+  if(sendCommand(cmd24) == 0x00){
+    getNextByte();// delay one byte
+
+    writeNextByte(0xFE); // data block header
+    for(int i =0; i < RW_BLOCK_SIZE; i++){
+      writeNextByte(buffer[i]);
+    }
+
+    //CRC is for chumps
+    getNextByte();
+    getNextByte();
+
+    //wait for response
+    for(int z = 0; z < TIMEOUT_CLK; z ++){
+      uint8_t r = getNextByte();
+      if((0x01 & (r >> 4)) == 0x00){
+        if((0x0F & r) == 0x5){
+          //block written!
+          waitForCard();
+          return true;
+        }
+        else {
+          break;
+        }
+      }
+    }
+    //fail for some reason
+    waitForCard();
+    return false;
+  }
+  else{
+    waitForCard();
     return false;
   }
 }
--- src/sdDriver.h
+++ src/sdDriver.h
@@ -4,9 +4,20 @@
 #include <stdint.h>
 
 #define TIMEOUT_CLK 16
+#define TIMEOUT_RW_BLOCK 16000
 #define RW_BLOCK_SIZE 512
 
+/**
+  init SD Card
+*/
 bool initDefault(void);
+
+/**
+  get card CSD register. put its value in buffer.
+  buffer MUST be 16 or more bytes.
+*/
+bool getCardCSD(uint8_t * buffer);
+#define CSD_RESPONSE_SIZE 16
 
 /**
   read a block of data from the SD card.
@@ -14,5 +25,12 @@ bool initDefault(void);
   addr is in blocks.
 */
 bool readBlock(uint8_t * buffer, uint32_t addr);
+
+
+/**
+  write block in buffer to addr.
+  buffer must be RW_BLOCK_SIZE
+*/
+bool writeBlock(uint8_t * buffer, uint32_t addr);
 
 #endif /* SD_DRIVER_H_ */
