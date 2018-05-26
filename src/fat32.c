87717a260bd9f10f98db5fd888ce3aeddadb20bc
Benjamin Benetti <ben.benetti@shaw.ca>
2018-05-26 00:20:16 -0700

    FAT work. + add support for MBR partition scheme. + add def that forces root directory to zero. On a disk partitioned by a Mac   the real root directory (often at cluster 2) contains a sql light database   (probs with some crazy Apple data structures. they love sql lite). The root,   root directory seems to be at cluster zero so ya..... little strange. + UCS2 to utf 8 decoding for VFAT entries.

--- src/fat32.c
+++ src/fat32.c
@@ -2,18 +2,32 @@
 #include "volume.h"
 #include <util/debug.h>
 #include <memory.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
 
 static Volume currVolume;
 
 static FATInfo fatInformation;
 
+static uint32_t __populateFD(ClusterIter * ci, uint16_t offset, FileDescriptor * fd);
+static uint32_t __decodeVFAT(ClusterIter * ci, uint16_t offset, char * fName);
+
+static void extractVFATName(wchar_t * dest, uint8_t * src);
 
 #define IS_GPT_PART(buff) (buff[0] == 0x45 && buff[1] == 0x46 && buff[2] == 0x49 && buff[3] == 0x20 && buff[4] == 0x50 \
   && buff[5] == 0x41 && buff[6] == 0x52 && buff[7] == 0x54)
 
+#define IS_MBR_PART(buff) (buff[510] == 0x55 && buff[511] == 0xaa)
+
+#define IS_MBR_PART_VALID(ptr) ((*ptr & 0x7F) == 0x00)
+
+#define IS_MBR_PART_FAT(ptr)(*(ptr + 0x4) & 0xfe)
+
 #define IS_FAT_PART_ENTRY(buff) (*(uint64_t*)(buff) == 0x4433b9e5ebd0a0a2 && \
   *(uint64_t*)(buff + 0x8) == 0xc79926b7b668c087)
 
+#define IS_VFAT_DIR_ENTRY(ptr) (*(ptr + 0xB) == 0x0F)
 
 static uint64_t locateFAT(){
   uint8_t buff[getVolumeBlockSize(&currVolume)];
@@ -33,9 +47,25 @@ static uint64_t locateFAT(){
         return *(uint64_t*)(buff + 128*(i%4) + 0x20);
       }
     }
+
+    return 0;
   }
   else {
-    return 0;
+    //attempt to read MBR (even GTP has backup MBR at 0x00)
+    readVolumeBlock(&currVolume, buff, 0);
+
+    if (IS_MBR_PART(buff)){
+      //four 16 byte partition entries (NO EBR support... come on if you want more than four just use GPT)
+      for(uint32_t i = 0x1BE; i < 0x1FE; i += 0x10){
+        if(IS_MBR_PART_VALID((buff + i)) && IS_MBR_PART_FAT((buff + i))){
+          return (uint64_t)(*(uint32_t*)(buff + i + 0x8));
+        }
+      }
+      return 0;
+    }
+    else {
+      return 0;
+    }
   }
 }
 
@@ -53,12 +83,11 @@ static void populateFATInformation(uint64_t fatStart){
   fatInformation.numFATs = *(buff + 0x10);
   fatInformation.blocksPerFAT = *(uint32_t*)(buff + 0x24);
   fatInformation.version = *(uint16_t*)(buff + 0x2a);
+#ifndef FORCE_ROOT_CLUSTER_ZERO
   fatInformation.rootCluster = *(uint32_t*)(buff + 0x2c);
-
-  readVolumeBlock(&currVolume, buff, fatInformation.prologBlocks + fatInformation.partitionStart +
-     fatInformation.blocksPerFAT*fatInformation.numFATs +
-     fatInformation.rootCluster*fatInformation.blocksPerCluster);
-  dumpHex(buff,512);
+#else
+  fatInformation.rootCluster = 0;
+#endif
 }
 
 void initFat32Driver(void){
@@ -71,6 +100,16 @@ void initFat32Driver(void){
   }
 
   populateFATInformation(fatAddress);
+
+  ClusterIter ci;
+  FileDescriptor fd;
+
+  newClusterIterator(fatInformation.rootCluster, &ci);
+  __populateFD(&ci, 32, &fd);
+
+  dumpHex(ci.blockData, 512);
+
+  printf("NAME: \"%s\" Cluster: %d\n", fd.fileName, fd.startCluster);
 }
 
 
@@ -86,4 +125,195 @@ void printFATInfo(void){
   printf("Num FATS: \t\t%d \n", fatInformation.numFATs);
   printf("Root Cluster: \t\t%d\n", fatInformation.rootCluster);
   printf("#### FAT INFO #####\n");
+}
+
+void readCluster(uint32_t cluster, uint16_t block, uint8_t * buffer){
+  uint32_t bAddr = fatInformation.prologBlocks + fatInformation.partitionStart +
+                   fatInformation.blocksPerFAT*fatInformation.numFATs +
+                   cluster*fatInformation.blocksPerCluster + block;
+  readVolumeBlock(&currVolume, buffer, bAddr);
+}
+
+uint32_t fetchNextCluster(uint32_t cluster){
+  uint32_t vblock = getVolumeBlockSize(&currVolume);
+  uint8_t buffer[vblock];
+  uint32_t fatAddress = fatInformation.prologBlocks + fatInformation.partitionStart + (cluster / vblock);
+
+  readVolumeBlock(&currVolume, buffer, fatAddress);
+
+  uint32_t nextCluster = *(buffer + (cluster % vblock));
+  if(nextCluster < 0x0fffffff){
+    return nextCluster;
+  }
+  else{
+    return 0;
+  }
+}
+
+bool getFirstFileInDirectory(char * path, FileDescriptor * fd){
+
+}
+
+bool getFirstFileInDirectoryCluster(uint32_t cluster, FileDescriptor * fd){
+
+}
+
+bool getNextFileInDirectory(FileDescriptor * fd){
+
+}
+
+static uint32_t __getDirectory(char * path);
+void getDirectory(char * path, FileDescriptor * fd){
+
+}
+
+static uint32_t __populateFD(ClusterIter * ci, uint16_t offset, FileDescriptor * fd){
+  if(IS_VFAT_DIR_ENTRY((ci->blockData + offset)) && *(ci->blockData + offset) & 0x40 ){
+    //first entry in a VFAT chain
+    uint32_t realEntryOffset = __decodeVFAT(ci, offset, fd->fileName);
+    return __populateFD(ci, realEntryOffset, fd);
+  }
+  else if (!IS_VFAT_DIR_ENTRY((ci->blockData + offset))){
+    //normal entry (name should have been populated from VFAT)
+    uint8_t * entryPtr = ci->blockData + offset;
+    fd->containingCluster = ci->currCluster;
+    fd->directoryEntryOffset = offset;
+    fd->startCluster =  ((uint32_t)*(entryPtr + 0x15)<<24) | ((uint32_t)*(entryPtr + 0x14)<<16) |
+                        ((uint32_t)*(entryPtr + 0x1B)<<8) | (uint32_t)*(entryPtr + 0x1A);
+    if(*(entryPtr + 0xB) & 0x10){
+      fd->fileType = FILE_TYPE_DIRECTORY;
+    }
+    else{
+      fd->fileType = FILE_TYPE_BINARY;
+    }
+  }
+  return offset;
+}
+
+static uint32_t __decodeVFAT(ClusterIter * ci, uint16_t offset, char * fName){
+  uint8_t entries[(FAT32_MAX_NAME_LEN/8)*FAT32_DIR_ENTRY_SIZE];
+  memset(entries, 0 , (FAT32_MAX_NAME_LEN/8)*FAT32_DIR_ENTRY_SIZE);
+  uint32_t countEntries = 0;
+
+  for(int i =offset; countEntries < (FAT32_MAX_NAME_LEN/8); i+=FAT32_DIR_ENTRY_SIZE){
+    if(IS_VFAT_DIR_ENTRY((ci->blockData + i))){
+      uint8_t * vFatPtr = ci->blockData + i;
+      wchar_t wName[14];
+      extractVFATName(wName, vFatPtr);
+      dumpHex((uint8_t*)wName,14*4);
+      wcstombs(entries + countEntries*FAT32_DIR_ENTRY_SIZE, wName, FAT32_DIR_ENTRY_SIZE-1);
+      printf("PART: %s \n", (char*)(entries + countEntries*FAT32_DIR_ENTRY_SIZE));
+      countEntries ++;
+    }
+    else {
+      //end of chain.
+      memset(fName, 0, FAT32_MAX_NAME_LEN);
+      for(int z = countEntries -1; z >= 0; z--){
+        strcat(fName, entries + z*FAT32_DIR_ENTRY_SIZE);
+      }
+      return i;
+    }
+
+    if(i + FAT32_DIR_ENTRY_SIZE >= getVolumeBlockSize(&currVolume)){
+      getNextBlockClusterIterator(ci);
+      i = 0;
+
+      if(ci->eof){
+        break;
+      }
+    }
+  }
+
+  return 0;
+}
+
+static uint32_t __getDirectory(char * path){
+  char subPath[strlen(path)];
+  memset(subPath, 0, strlen(path));
+  char * slashPtr = strrchr(path, '/');
+
+  if((uint32_t)slashPtr - (uint32_t)path > 0){
+    //there are directories bellow this one
+    memcpy(subPath, path, (uint32_t)slashPtr - (uint32_t)path);
+    uint32_t parentDir = __getDirectory(subPath);
+
+    if(parentDir > 0){
+      //look for dir
+      memset(subPath, 0, strlen(path));
+      strcpy(subPath, slashPtr);
+
+      FileDescriptor fd;
+      getFirstFileInDirectoryCluster(parentDir, &fd);
+
+      for(;;){
+        if(strcmp(fd.fileName,subPath) == 0 && fd.fileType == FILE_TYPE_DIRECTORY){
+          return fd.startCluster;
+        }
+        if(!getNextFileInDirectory(&fd)){
+          return 0;
+        }
+      }
+    }
+    else {
+      //error
+      return 0;
+    }
+  }
+  else {
+    //we must be in root directory
+    return fatInformation.rootCluster;
+  }
+}
+
+
+void newClusterIterator(uint32_t clusterStart, ClusterIter * ci){
+  ci->eof = false;
+  ci->currCluster = clusterStart;
+  ci->currBlock = 0;
+  ci->blockData = malloc(getVolumeBlockSize(&currVolume));
+
+  readCluster(ci->currCluster, ci->currBlock, ci->blockData);
+}
+
+void freeClusterIterator(ClusterIter * ci){
+  free(ci->blockData);
+}
+
+void getNextBlockClusterIterator(ClusterIter * ci){
+  if(ci->eof){
+    return;
+  }
+
+  if(ci->currBlock + 1 > fatInformation.blocksPerCluster - 1){
+    //fetch next cluster
+    ci->currCluster = fetchNextCluster(ci->currCluster);
+    ci->currBlock = 0;
+    if(ci->currCluster == 0){
+      ci->eof = true;
+    }
+  }
+  else {
+    ci->currBlock ++;
+    readCluster(ci->currCluster, ci->currBlock, ci->blockData);
+  }
+}
+
+static void extractVFATName(wchar_t * dest, uint8_t * src){
+  //NOTE wchar_t is 32 bit!!!!!! (It is 16 on disk)
+  uint8_t * p = src + 0x1;
+  for(uint16_t i = 0; i < 10; i+=2){
+    *dest++ = (uint32_t)((*(p + i)) | (*(p + i + 1) << 8));
+  }
+
+  p = src + 0x0E;
+  for(uint16_t i = 0; i < 12; i+=2){
+    *dest++ = (uint32_t)((*(p + i)) | (*(p + i + 1) << 8));
+  }
+
+  p = src + 0x1C;
+  for(uint16_t i = 0; i < 4; i+=2){
+    *dest++ = (uint32_t)((*(p + i)) | (*(p + i + 1) << 8));
+  }
+
+  *dest = 0x00;
 }
--- src/fat32.h
+++ src/fat32.h
@@ -1,6 +1,15 @@
 #ifndef FAT_32_H_
 #define FAT_32_H_
 #include <stdio.h>
+#include <stdbool.h>
+
+#define FAT32_MAX_NAME_LEN 128
+#define FAT32_DIR_ENTRY_SIZE 32
+
+/*** FAT32 file types ***/
+#define FILE_TYPE_DIRECTORY 0
+#define FILE_TYPE_BINARY    1
+#define FILE_TYPE_VFAT      2
 
 typedef struct {
   char      oemName[8];
@@ -14,6 +23,32 @@ typedef struct {
   uint64_t  partitionStart;
 } FATInfo;
 
+/**
+  I cannot find any information on this any where, but as far as
+  I can tell the root cluster field in the FAT "header" is (some what)
+  incorrect. It indicates root cluster as cluster 2. this cluster does contain
+  a directory but it does not contain files placed in the root. rather it contains
+  a sqlLite 3 database!!!! I suspect this is some Apple Bull Shit!
+
+  remove def to use root cluster value in FAT header
+*/
+#define FORCE_ROOT_CLUSTER_ZERO 
+
+typedef struct {
+  char fileName[FAT32_MAX_NAME_LEN];
+  // binary or directory
+  uint8_t fileType;
+
+  // first cluster of the directory that contains this file
+  uint32_t containingCluster;
+  // the offset of this entry from the start of the containing directory
+  // i.e. firstCluster + directoryEntryOffset*32 (because 32 bytes an entry)
+  uint32_t directoryEntryOffset;
+  // start cluster of this file or directory
+  uint32_t startCluster;
+
+} FileDescriptor;
+
 
 /**
   set up data structures and what not. DOES NOT AFFECT DISK
@@ -22,4 +57,50 @@ void initFat32Driver(void);
 
 
 void printFATInfo(void);
+
+void readCluster(uint32_t cluster, uint16_t block, uint8_t * buffer);
+
+/**
+  fetch the next cluster in the FAT given the current cluster.
+  or zero if EOF
+*/
+uint32_t fetchNextCluster(uint32_t cluster);
+
+/**
+  fill fd with information about the first file in the
+  directory denoted by path.
+  @return false if there are no files in directory (impossible because of . and .. )
+*/
+bool getFirstFileInDirectory(char * path, FileDescriptor * fd);
+/**
+  like getFirstFileInDirectory but uses a cluster index instead of a path to specify
+  the directory of interest
+*/
+bool getFirstFileInDirectoryCluster(uint32_t cluster, FileDescriptor * fd);
+
+/**
+  overwrite fd with the next file in the directory
+  @return false if there is no file after the one denoted by fd
+*/
+bool getNextFileInDirectory(FileDescriptor * fd);
+
+/**
+  get file descriptor for the directory indicated by path
+*/
+void getDirectory(char * path, FileDescriptor * fd);
+
+typedef struct {
+  uint32_t currCluster;
+  uint8_t currBlock;
+  bool eof;
+  uint8_t * blockData;
+} ClusterIter;
+
+// create a cluster iterator and fetch the first block
+void newClusterIterator(uint32_t clusterStart, ClusterIter * ci);
+void freeClusterIterator(ClusterIter * ci);
+
+// get the next block in the given cluster iterator
+void getNextBlockClusterIterator(ClusterIter * ci);
+
 #endif /*FAT_32_H_*/
